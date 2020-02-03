edf4ec4bfe5c239deb544e2284b6773fb445a146
Jan Nyman <jan.nyman@controlthings.fi>
2020-02-03 16:03:07 +0200

    Add license banner

--- ios_port/port_service_ipc.c
+++ ios_port/port_service_ipc.c
@@ -1,3 +1,14 @@
+/**
+ * Copyright (C) 2020, ControlThings Oy Ab
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * @license Apache-2.0
+ */
 /*
  * A dummy, or "faux" Service IPC layer implementation, 
  * which exists only to tie services togheter with the core in the
